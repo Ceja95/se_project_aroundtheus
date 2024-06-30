929ba4d8288b52322cdf93e455490301d22e4372
Ceja95 <laracejaj@gmail.com>
2024-06-30 05:20:20 -0700

    fix nested repos

---
+++ README.md
@@ -0,0 +1,26 @@
+# Project 3: Around The U.S.
+
+### Overview  
+
+*Intro*
+*Name*
+*Description*
+*Improvment*
+
+## Intro:
+This is a website of a customer who has travled around the world and shares where he has travled to, which you can give him a heart like.
+
+## Name:
+The name of this project is called Around the U.S.
+
+## Description:
+In this project it uses a simple html format which contains a header of the projects name, a image of the customer and his name along with the edit functionality in cause he wants to update and of his info. We also included a add button to where you can add more profile images of your recent traveled locations.  There's the image section where you can see all your updated pictures organized in 3 columns and 2 rows till you go down to about 895 width for tablet use it turns into a 2 column and 3 row website then when you get to mobile use at about 320px it turns to 1 single column. There is a description section for the image and to the side there is a like button which is a heart image which will chage color when hovered over.
+
+## Improvement:
+For my lists of improvments im going to add a differnt background image one that matches the customers hobbies or images that he uploads into the page. im going to add a contacts and nav links to the users social profiles. 
+
+## Deployment:
+https://ceja95.github.io/se_project_aroundtheus/
+
+## Video:
+https://drive.google.com/file/d/1dWSmWuvCW1X-JMgil8pw1a4iR-zX94W1/view?usp=sharing
---
+++ scripts/index.js
@@ -0,0 +1,90 @@
+const object1 = {
+  name: "Yosemite Valley",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  alt: "picture of yosemite valley",
+};
+
+const object2 = {
+  name: "Lake Louise",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+  alt: "picture of lake louise"
+};
+
+const object3 = {
+  name: "Bald Mountains",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  alt: "picture of bald mountians",
+};
+
+const object4 = {
+  name: "Latemar",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  alt: "picture of latemar",
+};
+
+const object5 = {
+  name: "Vanoise National Park",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+  alt: "picture of vanoise national park",
+};
+
+const object6 = {
+  name: "Lago di Braies",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+  alt: "picture of lago di braies",
+};
+
+const initialCards = [object1, object2, object3, object4, object5, object6];
+
+console.log(initialCards);
+
+const profileEditButton = document.querySelector(".profile__edit-button");
+const profileEditModal = document.querySelector(".modal");
+const profileCloseButton = document.querySelector(".modal__close");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__paragraph");
+const profileTitleInput = document.querySelector("#profile-name-input");
+const profileDescriptionInput = document.querySelector("#profile-description-input");
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardTemplate = document.querySelector(".card-template").content.firstElementChild;
+const cardListEle = document.querySelector(".cards__list");
+
+function closePopup() {
+  profileEditModal.classList.remove("modal_opened");
+}
+
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageEle = cardElement.querySelector(".card__image");
+  const cardDescriptionEle = cardElement.querySelector(".card__header");
+
+  cardDescriptionEle.textContent = cardData.name;
+  cardImageEle.src = cardData.link;
+  cardImageEle.alt = cardData.alt;
+
+  return cardElement;
+}
+
+profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
+  profileEditModal.classList.add("modal_opened");
+});
+
+profileCloseButton.addEventListener("click", () => {
+  closePopup();
+});
+
+profileEditForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopup();
+});
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListEle.append(cardElement);
+});
+
+
