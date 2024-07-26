8eb180d9dfe5fb92c670cf7ab023dc7640647db6
Ceja95 <laracejaj@gmail.com>
2024-07-25 17:14:59 -0700

    add error message

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,27 +1,43 @@
+function showInputError(formEl, inputEl, config) {
+  const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
+  console.log(errorMessageEl);
+}
+
+function checkInputValidity(formEl, inputEl, config) {
+  if (!inputEl.validity.valid) {
+    showInputError(formEl, inputEl, config);
+  } else {
+    hideInputError(formEl, inputEl, config);
+  }
+}
+
 function setEventListeners(formEl, config) {
-    const {inputSelector} = config;
-const inputEls = [...formEl.querySelectorAll(inputSelector)];
-console.log(inputEls);
+  const { inputSelector } = config;
+  const inputEls = [...formEl.querySelectorAll(inputSelector)];
+  inputEls.forEach((inputEl) => {
+    inputEl.addEventListener("input", () => {
+      checkInputValidity(formEl, inputEl, config);
+    });
+  });
 }
 
 function enableValidation(config) {
-    const formEls = [...document.querySelectorAll(config.formSelector)];
-    formEls.forEach((formEl) => {
-        formEl.addEventListener("submit", (e) => {
-            e.preventDefault();
-        })
-    })
+  const formEls = [...document.querySelectorAll(config.formSelector)];
+  formEls.forEach((formEl) => {
+    formEl.addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
+    setEventListeners(formEl, config);
+  });
 }
 
-setEventListeners(formEl, config);
-
 const config = {
-    formSelector: ".modal__form",
-    inputSelector: ".popup__input",
-    submitButtonSelector: ".popup__button",
-    inactiveButtonClass: "popup__button_disabled",
-    inputErrorClass: "popup__input_type_error",
-    errorClass: "popup__error_visible"
-  }
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible",
+};
 
 enableValidation(config);
