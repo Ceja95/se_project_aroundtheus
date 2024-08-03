1e30b97f903453cfeb04f7db9e006e1422e6ef7e
Ceja95 <laracejaj@gmail.com>
2024-08-02 17:24:12 -0700

    added span for desctiprtion

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,7 +1,17 @@
-function showInputError(formEl, inputEl, config) {
+function showInputError(formEl, inputEl, {inputErrorClass, errorClass}) {
   const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
-  console.log(errorMessageEl);
+  inputEl.classList.add(inputErrorClass);
+  errorMessageEl.textContent = inputEl.validationMessage;
+  errorMessageEl.classList.add(errorClass);
 }
+
+function hideInputError(formEl, inputEl, {inputErrorClass, errorClass}) {
+  const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
+  inputEl.classList.remove(inputErrorClass);
+  errorMessageEl.textContent = "";
+  errorMessageEl.classList.remove(errorClass);
+}
+
 
 function checkInputValidity(formEl, inputEl, config) {
   if (!inputEl.validity.valid) {
@@ -11,12 +21,32 @@ function checkInputValidity(formEl, inputEl, config) {
   }
 }
 
+function toggleButtonState(inputEls, submitButton, {inactiveButtonClass}) {
+  let foundInvalid = false;
+  inputEls.forEach(inputEl => {
+    if(!inputEl.validity.valid) {
+      foundInvalid = true;
+    }
+  });
+
+  if(foundInvalid) {
+    submitButton.classList.add(inactiveButtonClass);
+    submitButton.disabled = true;
+  }else {
+    submitButton.classList.remove(inactiveButtonClass);
+    submitButton.disabled = false;
+  }
+}
+
 function setEventListeners(formEl, config) {
   const { inputSelector } = config;
   const inputEls = [...formEl.querySelectorAll(inputSelector)];
+  const submitButton = formEl.querySelector(".modal__button");
+
   inputEls.forEach((inputEl) => {
     inputEl.addEventListener("input", () => {
       checkInputValidity(formEl, inputEl, config);
+      toggleButtonState(inputEls, submitButton, config);
     });
   });
 }
@@ -34,8 +64,8 @@ function enableValidation(config) {
 const config = {
   formSelector: ".modal__form",
   inputSelector: ".modal__input",
-  submitButtonSelector: ".popup__button",
-  inactiveButtonClass: "popup__button_disabled",
+  submitButtonSelector: ".modal__button",
+  inactiveButtonClass: "modal__button_disabled",
   inputErrorClass: "popup__input_type_error",
   errorClass: "popup__error_visible",
 };
