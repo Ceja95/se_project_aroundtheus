bb469bff1f1fc233d274cb4097bb5a9e61c21c25
Ceja95 <laracejaj@gmail.com>
2024-10-04 12:48:18 -0700

    fixed open/close method working on setEvent method

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -2,22 +2,22 @@ export class Popup {
   constructor({popupSelector}) {
     this._popupElement = document.querySelector(popupSelector);
   }
-  open(modal) {
-    modal.classList.add("modal_opened");
+  open() {
+    this._popupElement.classList.add("modal_opened");
     document.addEventListener("keydown", this._handleEscapeClose);
   }
-  close(modal) {
-    modal.remove("modal_opened");
+  close() {
+    this._popupElement.remove("modal_opened");
     document.removeEventListener("keydown", this._handleEscapeClose);
   }
   _handleEscapeClose(e) {
     if (e.key === "Escape") {
-      this.close(this._popupElement);
+      this.close();
     }
   }
   setEventListeners() {
     this._popupElement.querySelector(".modal__close").addEventListener("click", () => {
-      this.close(this._popupElement);
+      this.close();
     });
 
     this._popupElement.addEventListener("keydown", this._handleEscapeClose);
--- src/components/PopupWithForms.js
+++ src/components/PopupWithForms.js
@@ -2,7 +2,7 @@ import {Popup} from "./Popup.js";
 
 export default class PopupWithForm extends Popup {
     constructor(popupSelector, handleFormSubmit) {
-      super(popupSelector);
+      super({popupSelector});
       this._handleFormSubmit = handleFormSubmit;
     }
     _getInputValues() {}
