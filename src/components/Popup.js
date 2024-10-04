2fea033b5bf74fb31490e63bd43c4080b835556c
Ceja95 <laracejaj@gmail.com>
2024-10-04 11:47:05 -0700

    fixing open/close methods

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -2,12 +2,12 @@ export class Popup {
   constructor({popupSelector}) {
     this._popupElement = document.querySelector(popupSelector);
   }
-  open() {
-    this._popupElement.classList.add("modal_opened");
+  open(modal) {
+    modal.classList.add("modal_opened");
     document.addEventListener("keydown", this._handleEscapeClose);
   }
-  close() {
-    this._popupElement.classList.remove("modal_opened");
+  close(modal) {
+    modal.remove("modal_opened");
     document.removeEventListener("keydown", this._handleEscapeClose);
   }
   _handleEscapeClose(e) {
