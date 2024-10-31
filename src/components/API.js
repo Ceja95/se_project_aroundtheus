308962e847c481b330ef9defbedb996f08aeb5f4
Ceja95 <laracejaj@gmail.com>
2024-10-30 17:59:18 -0700

    class API and method added

---
+++ src/components/API.js
@@ -0,0 +1,26 @@
+class Api {
+    constructor(options) {
+        this._options = options;
+    }
+
+    getInitialCards() {
+        return fetch("https://around-api.en.tripleten-services.com/v1/cards", {
+            headers: {
+            authorization: "d43d74f4-d8b7-495e-a8b5-9bce636b1f9e"
+            }
+        })
+        .then(res => {
+            if(res.ok) {
+                return res.json();
+            }
+        });
+    }
+
+    const api = new Api({
+        baseUrl: "https://around-api.en.tripleten-services.com/v1",
+        headers: {
+            authorization: "d43d74f4-d8b7-495e-a8b5-9bce636b1f9e",
+            "Content-Type": "application/json"
+        }
+    });
+}
