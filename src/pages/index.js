75bbfc675dd91cce7c7849c41f163ef0679dc543
Ceja95 <laracejaj@gmail.com>
2025-03-25 14:35:09 -0700

    fixing confirm modal

--- src/pages/index.js
+++ src/pages/index.js
@@ -51,13 +51,11 @@ const editPopupForm = new PopupWithForm(
 );
 editPopupForm.setEventListeners();
 
-const confirmDelete = new PopupWithConfirm(
-  { popupSelector: "#confirm-modal" },
+const confirmDelete = new PopupWithConfirm("#confirm-modal",
   (itemToDelete) => {
     console.log("deleting", itemToDelete);
     itemToDelete.handleDeleteCard();
-  }
-)
+  });
 confirmDelete.setEventListeners();
 
 const imagePopup = new PopupWithImage({ popupSelector: "#image-modal" });
