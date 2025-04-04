6d66112ef9ec2b20444f1d96d42179358061e8c6
Ceja95 <laracejaj@gmail.com>
2025-04-04 16:56:42 -0700

    fixed confirm modal not closing when clicked on overlay

--- src/components/PopupWithConfirm.js
+++ src/components/PopupWithConfirm.js
@@ -5,7 +5,7 @@ export default class PopupWithConfirm extends PopupWithForm {
         super({ popupSelector }, handleConfirmAction);
         this._handleConfirmAction = handleConfirmAction;
         this._submitBtn = this._popupElement.querySelector(".modal__button");
-        this._submitBtnContent = this._submitBtn.textContent; 
+        this._submitBtnContent = this._submitBtn.textContent;
     }
 
     open(itemToDelete) {
@@ -14,23 +14,12 @@ export default class PopupWithConfirm extends PopupWithForm {
     }
 
     setEventListeners() {
+        super.setEventListeners();
         this._confirmButton = this._popupElement.querySelector("#confirm-button");
-        this._rejectButton = this._popupElement.querySelector("#confirm-reject");
-        this._closeButton = this._popupElement.querySelector("#confirm-close"); 
 
         this._confirmButton.addEventListener("click", (e) => {
-            e.preventDefault();
-            this._handleConfirmAction(this._itemToDelete);
-            this.close();
-        });
-
-        this._rejectButton.addEventListener("click", (e) => {
-            e.preventDefault();
-            this.close();
-        });
-
-        this._closeButton.addEventListener("click", (e) => {
-            e.preventDefault();
+           e.preventDefault();
+           this._handleConfirmAction(this._itemToDelete);
             this.close();
         });
     }
